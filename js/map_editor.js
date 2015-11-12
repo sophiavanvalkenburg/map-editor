746de16746607ef02c473af96d47c4617504198d
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-11-12 15:23:06 -0500

    some refactoring

--- js/map_editor.js
+++ js/map_editor.js
@@ -87,88 +87,119 @@ Canvas.prototype.tileGridOutput = function(){
 }
 
 
-function convertToInt(str){
-  var new_int = parseInt(str);
-  if (isNaN(new_int)){
-    console.log("err: " + str + " is not a number");
-    return 0;
+var MapEditor = function(){
+  this.setup();
+}
+MapEditor.prototype.generateCanvas = function(){
+  var resolution = Utils.convertToInt($("#tile-resolution").val());
+  var num_columns = Utils.convertToInt($('#canvas-num-cols').val());
+  var num_rows = Utils.convertToInt($('#canvas-num-rows').val());  
+  this.canvas = new Canvas("canvas", resolution, num_columns, num_rows);
+  this.canvas.draw();
+  $("#palette").show();
+  this.setupTileClickHandler();
+}
+MapEditor.prototype.setupTileClickHandler = function(){
+  var the_editor = this;
+  $(".tile").click(
+    function(){
+      var tile = the_editor.canvas.getTile($(this).data("x"), $(this).data("y"));
+      switch(the_editor.canvas.getMode()){
+        case Canvas.DRAW:
+          the_editor.setTileColor(tile);
+          break;
+        case Canvas.ERASER:
+          the_editor.eraseTileColor(tile);
+          break;
+        case Canvas.DROPPER:
+          the_editor.getTileColor(tile);
+          break;
+      }
+    });
+}
+MapEditor.prototype.changeModes = function($clicked_tool){
+  var canvas_mode = this.canvas.getMode();
+  var clicked_mode = $clicked_tool.data("mode");
+  if (canvas_mode === clicked_mode){
+    this.canvas.setMode(Canvas.DRAW);
+    $clicked_tool.removeClass("activated");
+  }else{
+    $(".tools-draw").removeClass("activated");
+    $clicked_tool.addClass("activated");
+    this.canvas.setMode(clicked_mode);
   }
-  return new_int;
+}
+MapEditor.prototype.toggleGridlines = function($clicked_tool){
+  $clicked_tool.toggleClass("activated");
+  if ($clicked_tool.hasClass("activated")){
+    $("#canvas td").addClass("show-gridlines");
+  }else{
+    $("#canvas td").removeClass("show-gridlines");
+  }
+}
+MapEditor.prototype.saveMap = function(){
+  var output = this.canvas.tileGridOutput();
+  var encoded_uri = encodeURI(output);
+  window.open(encoded_uri);
+}
+MapEditor.prototype.setupPalette = function(){
+ $("#palette .picker").spectrum({
+    color: "#f00",
+    });
+  $("#eraser").data("mode", Canvas.ERASER);
+  $("#dropper").data("mode", Canvas.DROPPER);
+}
+MapEditor.prototype.setup = function(){
+  var canvas = undefined;
+  var the_editor = this;
+  this.setupPalette();
+  $('#generate-canvas-btn').click(
+    function() {
+      the_editor.generateCanvas();
+    });
+  $(".tools-draw").click(
+    function(){
+      the_editor.changeModes($(this));
+    });
+  $("#gridlines").click(
+    function(){
+      the_editor.toggleGridlines($(this))
+    });
+  $("#save").click(
+    function(){
+      the_editor.saveMap();
+    });
 }
 
-function setTileColor(tile){
+MapEditor.prototype.setTileColor = function(tile){
   var current_color = $("#palette .picker").spectrum("get").toHexString();
   tile.setColor(current_color);
   tile.draw();
 }
 
-function eraseTileColor(tile){
+MapEditor.prototype.eraseTileColor = function(tile){
   tile.unsetColor();
   tile.draw();
 }
 
-function getTileColor(tile){
+MapEditor.prototype.getTileColor = function(tile){
   $("#palette .picker").spectrum("set", tile.color);
   $("#dropper").click();
 }
 
+
+var Utils = {
+  convertToInt: 
+    function(str){
+      var new_int = parseInt(str);
+      if (isNaN(new_int)){
+        console.log("err: " + str + " is not a number");
+        return 0;
+      }
+      return new_int;
+    }
+}
+
 $(function(){
-  var canvas = undefined;
-  $('#generate-canvas-btn').click(
-    function(){
-      var resolution = convertToInt($("#tile-resolution").val());
-      var num_columns = convertToInt($('#canvas-num-cols').val());
-      var num_rows = convertToInt($('#canvas-num-rows').val());  
-      canvas = new Canvas("canvas", resolution, num_columns, num_rows);
-      canvas.draw();
-      $("#palette").show();
-      $(".tile").click(
-        function(){
-          var tile = canvas.getTile($(this).data("x"), $(this).data("y"));
-          switch(canvas.getMode()){
-            case Canvas.DRAW:
-              setTileColor(tile);
-              break;
-            case Canvas.ERASER:
-              eraseTileColor(tile);
-              break;
-            case Canvas.DROPPER:
-              getTileColor(tile);
-              break;
-          }
-        });
-    });
-  $("#palette .picker").spectrum({
-    color: "#f00",
-    });
-  $("#eraser").data("mode", Canvas.ERASER);
-  $("#dropper").data("mode", Canvas.DROPPER);
-  $(".tools-draw").click(
-    function(){
-      var canvas_mode = canvas.getMode();
-      var this_mode = $(this).data("mode");
-      if (canvas_mode === this_mode){
-        canvas.setMode(Canvas.DRAW);
-        $(this).removeClass("activated");
-      }else{
-        $(".tools-draw").removeClass("activated");
-        $(this).addClass("activated");
-        canvas.setMode(this_mode);
-      }
-    });
-  $("#gridlines").click(
-    function(){
-      $(this).toggleClass("activated");
-      if ($(this).hasClass("activated")){
-        $("#canvas td").addClass("show-gridlines");
-      }else{
-        $("#canvas td").removeClass("show-gridlines");
-      }
-    });
-  $("#save").click(
-    function(){
-      var output = canvas.tileGridOutput();
-      var encoded_uri = encodeURI(output);
-      window.open(encoded_uri);
-    });
+ new MapEditor();
 });
